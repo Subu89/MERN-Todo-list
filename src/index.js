2ab18e7657c55ba05b7b0b3e48dde3327e1cdccf
Subu89 <sudipmhnt@gmail.com>
2022-08-13 19:22:32 +0530

    added CRUD routes to backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const todosRoutes = require('./routes/todos.routes');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/rest/todos', todosRoutes);
 
 app.use('/', (req, res) => {
     res.status(200).send('<h1>Hello World!</h1>')
